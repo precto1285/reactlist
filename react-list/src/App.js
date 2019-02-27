45df993d85a6392456f968857269b848fe4f6116
Philippe Recto <precto1285@gmal.com>
2019-02-26 16:54:38 -0800

    added state

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -6,28 +6,37 @@ import Footer from './Components/Footer';
 import Entry from './Components/Entry';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'Grocery List',
+      newItem: 'Milk',
+      items: 'Fish',
+      footer: 'Thank You For Visiting!'
+    }
+  }
+
   render() {
     return (
       <div className="App container">
         <Header
-          title="Grocery List"></Header>
-
+          title={this.state.title}></Header>
         <br />
         <div className="row">
           <div className="col-md-6">
             <Entry
-              newitem="Milk"></Entry>
+              newitem={this.state.newItem}></Entry>
           </div>
           <br />
           <div className="col-md-6">
             <Listarea
-              items="Fish"></Listarea>
+              items={this.state.items}></Listarea>
           </div>
         </div>
         <br />
         <Footer
-          footer="Thank You For Visiting!"></Footer>
-      </div>
+          footer={this.state.footer}></Footer>
+      </div >
     );
   }
 }
